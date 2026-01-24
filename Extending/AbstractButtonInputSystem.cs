9945d3a3ae7c972bf60f7545fbf12cd5fa3f982e
DarkMorninG <Bustercall@gmx.net>
2026-01-24 21:01:37 +0100

    added functioning for the new inputsystem from unity

---
+++ Extending/AbstractButtonInputSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DBH.Input.api.Extending {
+    public abstract class AbstractButtonInputSystem {
+        public delegate void ButtonPressed();
+
+        public event ButtonPressed OnButtonPressed;
+
+        protected abstract string Path { get; }
+
+        protected AbstractButtonInputSystem() {
+            var inputAction = InputSystem.actions.FindAction(Path);
+            if (inputAction == null) {
+                Debug.LogError("missing Input Action For:" + Path);
+            } else {
+                inputAction.performed += OnButtonPerformed;
+            }
+        }
+
+        private void OnButtonPerformed(InputAction.CallbackContext obj) {
+            OnButtonPressed?.Invoke();
+        }
+    }
+}
---
+++ Extending/AbstractValueInputSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DBH.Input.api.Extending {
+    public abstract class AbstractValueInputSystem<T> where T : struct {
+        public delegate void ButtonPressed(T value);
+
+        public event ButtonPressed OnButtonPressed;
+
+        protected abstract string Path { get; }
+
+        protected AbstractValueInputSystem() {
+            var inputAction = InputSystem.actions.FindAction(Path);
+            if (inputAction == null) {
+                Debug.LogError("missing Input Action For:" + Path);
+            } else {
+                inputAction.performed += OnButtonPerformed;
+            }
+        }
+
+        private void OnButtonPerformed(InputAction.CallbackContext obj) {
+            var readValue = obj.ReadValue<T>();
+            OnButtonPressed?.Invoke(readValue);
+        }
+    }
+}
