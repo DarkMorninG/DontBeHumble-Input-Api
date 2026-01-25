446bd1f1710df1c48c300be8e40e9109f0ff4a22
DarkMorninG <Bustercall@gmx.net>
2026-01-25 11:22:26 +0100

    added events for pressed and released button

--- Extending/AbstractButtonInputSystem.cs
+++ Extending/AbstractButtonInputSystem.cs
@@ -3,9 +3,11 @@ using UnityEngine.InputSystem;
 
 namespace DBH.Input.api.Extending {
     public abstract class AbstractButtonInputSystem {
-        public delegate void ButtonPressed();
+        public delegate void ButtonInput();
 
-        public event ButtonPressed OnButtonPressed;
+        public event ButtonInput OnButtonPerformed;
+        public event ButtonInput OnButtonStarted;
+        public event ButtonInput OnButtonCanceled;
 
         protected abstract string Path { get; }
 
@@ -14,12 +16,20 @@ namespace DBH.Input.api.Extending {
             if (inputAction == null) {
                 Debug.LogError("missing Input Action For:" + Path);
             } else {
-                inputAction.performed += OnButtonPerformed;
+                inputAction.performed += ButtonPerformed;
+                inputAction.started += ButtonStarted;
+                inputAction.canceled += ButtonCanceled;
             }
         }
 
-        private void OnButtonPerformed(InputAction.CallbackContext obj) {
-            OnButtonPressed?.Invoke();
+        private void ButtonPerformed(InputAction.CallbackContext obj) {
+            OnButtonPerformed?.Invoke();
+        }
+        private void ButtonStarted(InputAction.CallbackContext obj) {
+            OnButtonStarted?.Invoke();
+        }
+        private void ButtonCanceled(InputAction.CallbackContext obj) {
+            OnButtonCanceled?.Invoke();
         }
     }
 }
